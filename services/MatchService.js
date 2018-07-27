5dffe60e8d57e5c4d79b976b166dceb3101b8be5
Adi Gonnen <adigonnen@gmail.com>
2018-07-27 09:29:47 +0300

    marge frontend

--- services/MatchService.js
+++ services/MatchService.js
@@ -5,18 +5,59 @@ function queryMatch(userId) {
     var criteria = {};
     if (userId) criteria.$or = [
         {'userActive.userId': userId},
-        {'userPasive.userId': userId}
+        {'userPassive.userId': userId}
     ]
     return MongoService.connect()
         .then(db => {
             const collection = db.collection('match');
-            return collection.find(criteria).toArray()
+            return collection.aggregate([
+                {
+                    $match: criteria
+                },
+                {
+                    $lookup: {
+                        from: 'game',
+                        localField: 'userPassive.gameId',
+                        foreignField: '_id',
+                        as: 'userPassiveGame'
+                    }
+                },
+                {
+                    $unwind: '$userPassiveGame'
+                },
+                {
+                    $lookup: {
+                        from: 'game',
+                        localField: 'userActive.games',
+                        foreignField: '_id',
+                        as: 'userActiveGames'
+                    }
+                }
+            ]).toArray()
             .then(res => {
                 console.log('in querymatch got:', res)
                 return res
             })
         })
 }
+
+
+// function queryMatch(userId) {
+//     var criteria = {};
+//     if (userId) criteria.$or = [
+//         {'userActive.userId': userId},
+//         {'userpassive.userId': userId}
+//     ]
+//     return MongoService.connect()
+//         .then(db => {
+//             const collection = db.collection('match');
+//             return collection.find(criteria).toArray()
+//             .then(res => {
+//                 console.log('in querymatch got:', res)
+//                 return res
+//             })
+//         })
+// }
 
 // function getById(matchId) {
 //     matchId = new ObjectId(matchId)
